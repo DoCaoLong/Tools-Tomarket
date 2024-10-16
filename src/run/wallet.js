bb86109bf4252c85795baf1138511e07177c610d
Đỗ Cao Long <long@myspa.vn>
2024-10-16 13:29:24 +0700

    [CREATE] init

---
+++ src/run/wallet.js
@@ -0,0 +1,113 @@
+import colors from "colors";
+import dayjs from "dayjs";
+import delayHelper from "../helpers/delay.js";
+import fileHelper from "../helpers/file.js";
+import generatorHelper from "../helpers/generator.js";
+import authService from "../services/auth.js";
+import server from "../services/server.js";
+import userService from "../services/user.js";
+import walletService from "../services/wallet.js";
+
+const VERSION = "v1.0.0";
+// Đặt số lần thử kết nối lại tối đa khi proxy lỗi, nếu thử lại quá số lần cài đặt sẽ dừng chạy tài khoản đó và ghi lỗi vào file log
+const MAX_RETRY_PROXY = 20;
+// Đặt số lần thử đăng nhập tối đa khi đăng nhập lỗi, nếu thử lại quá số lần cài đặt sẽ dừng chạy tài khoản đó và ghi lỗi vào file log
+const MAX_RETRY_LOGIN = 20;
+
+let database = {};
+setInterval(async () => {
+  const data = await server.getData();
+  if (data) {
+    database = data;
+    server.checkVersion(VERSION, data);
+  }
+}, generatorHelper.randomInt(20, 40) * 60 * 1000);
+
+const run = async (user, index) => {
+  let countRetryProxy = 0;
+  let countRetryLogin = 0;
+  // await delayHelper.delay((user.index - 1) * DELAY_ACC);
+  while (true) {
+    // Lấy lại dữ liệu từ server
+    if (database?.ref) {
+      user.database = database;
+    }
+
+    // Kiểm tra kết nối proxy
+    let isProxyConnected = false;
+    while (!isProxyConnected) {
+      const ip = await user.http.checkProxyIP();
+      if (ip === -1) {
+        user.log.logError(
+          "Proxy lỗi, kiểm tra lại kết nối proxy, sẽ thử kết nối lại sau 30s"
+        );
+        countRetryProxy++;
+        if (countRetryProxy >= MAX_RETRY_PROXY) {
+          break;
+        } else {
+          await delayHelper.delay(30);
+        }
+      } else {
+        countRetryProxy = 0;
+        isProxyConnected = true;
+      }
+    }
+    try {
+      if (countRetryProxy >= MAX_RETRY_PROXY) {
+        const dataLog = `[No ${user.index} _ ID: ${
+          user.info.id
+        } _ Time: ${dayjs().format(
+          "YYYY-MM-DDTHH:mm:ssZ[Z]"
+        )}] Lỗi kết nối proxy - ${user.proxy}`;
+        fileHelper.writeLog("log.error.txt", dataLog);
+        break;
+      }
+
+      if (countRetryLogin >= MAX_RETRY_LOGIN) {
+        const dataLog = `[No ${user.index} _ ID: ${
+          user.info.id
+        } _ Time: ${dayjs().format(
+          "YYYY-MM-DDTHH:mm:ssZ[Z]"
+        )}] Lỗi đăng nhập thất bại quá ${MAX_RETRY_LOGIN} lần`;
+        fileHelper.writeLog("log.error.txt", dataLog);
+        break;
+      }
+    } catch (error) {
+      user.log.logError("Ghi lỗi thất bại");
+    }
+
+    // Đăng nhập tài khoản
+    const login = await authService.handleLogin(user, true);
+    if (!login.status) {
+      countRetryLogin++;
+      await delayHelper.delay(60);
+      continue;
+    } else {
+      countRetryLogin = 0;
+    }
+
+    await walletService.handleWallet(user);
+
+    break;
+  }
+};
+
+console.log(
+  colors.yellow.bold(
+    `=============  LDC  =============`
+  )
+);
+console.log("");
+console.log("");
+
+server.checkVersion(VERSION);
+server.showNoti();
+console.log("");
+const users = await userService.loadUser();
+
+for (const [index, user] of users.entries()) {
+  await run(user, index);
+}
+
+console.log(colors.magenta("Đã chạy xong!"));
+process.exit();
